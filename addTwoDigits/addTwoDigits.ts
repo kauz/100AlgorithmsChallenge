8658cd1a9565aa341528ac5520481548b767e0f6
kauz <kauzlein@gmail.com>
2019-01-31 18:13:24 +0300

    addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,5 @@
-function addTwoDigits(n: any): number {
-
+function addTwoDigits(n: number): any {
+    return (n + '').split('').reduce((accumulator: number, currentValue: string) => accumulator + Number(currentValue), 0);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
