3e3e846cb1b4f5e6ff9d696663368e2c2fab2b6e
kauz <kauzlein@gmail.com>
2019-01-31 18:54:26 +0300

    adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,31 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+// i've forgotten, that we need largest for adjacent numbers.
+function biggestElementsProduct(inputArray: number[]): number {
 
+    let x: number = Math.max(...inputArray);
+    let y: number = 0;
+    let i: number = 0;
+
+    while (i < 2) {
+        y = Math.max(...inputArray);
+        inputArray = inputArray.filter(n => n !== y);
+        i++;
+    }
+
+    return x * y;
 }
 
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(biggestElementsProduct([3, 6, -2, -5, 7, 3, 9]));
+
+function adjacentElementsProduct(inputArray: number[]): number {
+    let largestProduct = inputArray[0] * inputArray[1];
+
+    for (let i = 1; i < inputArray.length - 1; i++) {
+        let product = inputArray[i] * inputArray[i + 1];
+
+        largestProduct = largestProduct < product ? product : largestProduct;
+    }
+
+    return largestProduct;
+}
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3, 9]));
