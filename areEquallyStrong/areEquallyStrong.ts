02c5300ab945cc59ee485c4da66ee6b9a042542a
kauz <kauzlein@gmail.com>
2019-02-01 19:21:15 +0300

    areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,14 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
 
+    if (Math.min(yourLeft, yourRight) === Math.min(friendsLeft, friendsRight)) {
+        if (Math.max(yourLeft, yourRight) === Math.max(friendsLeft, friendsRight)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
