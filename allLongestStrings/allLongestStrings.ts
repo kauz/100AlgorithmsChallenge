e8bd30c38514f25cf70fa84b9337f73ad41ec5b5
kauz <kauzlein@gmail.com>
2019-02-01 16:07:57 +0300

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,12 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    let longest: number = 0;
+    for (let i: number = 0; i < inputArray.length - 1; i++) {
+        longest = inputArray[i].length > inputArray[i + 1].length
+            ? inputArray[i].length
+            : inputArray[i + 1].length;
+    }
 
+    return inputArray.filter((n: string) => n.length === longest);
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
