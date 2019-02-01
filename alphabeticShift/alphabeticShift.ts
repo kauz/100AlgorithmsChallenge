1fdead9a69df4ff2d15d50051a21f00c245d6875
kauz <kauzlein@gmail.com>
2019-02-01 17:02:49 +0300

    alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,12 @@
 function alphabeticShift(inputString: string): string {
 
+    let arr: number[] = [];
+    inputString.split('').forEach((char: string, idx: number) => {
+        arr[idx] = char.charCodeAt(0) + 1;
+        arr[idx] = arr[idx] > 122 ? 97 : arr[idx];
+    });
+
+    return String.fromCharCode(...arr);
 }
 
 console.log(alphabeticShift('crazy'));
