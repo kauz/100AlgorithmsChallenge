f06e5b09fa6075f9a016e4ad8938416860838698
kauz <kauzlein@gmail.com>
2019-02-01 18:14:33 +0300

    alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,20 @@
 function alphabetSubsequence(s: string): boolean {
+    let arr: number[] = [];
 
+    s.toLowerCase().split('').forEach((char: string, idx: number) => {
+        arr[idx] = char.charCodeAt(0);
+    });
+
+    for (let i: number = 0; i < arr.length - 1; i++) {
+        if (arr[i] < 97 || arr[i] > 122 || arr[i] >= arr[i + 1]) {
+            return false;
+        }
+    }
+
+    return true;
 }
-
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab'));
+console.log(alphabetSubsequence('effg'));
+console.log(alphabetSubsequence('cdce'));
+console.log(alphabetSubsequence('ace'));
+console.log(alphabetSubsequence('bxz'));
