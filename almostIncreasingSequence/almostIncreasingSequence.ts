d37a50a6fd71d87bb6ac689828076a8318905306
kauz <kauzlein@gmail.com>
2019-02-01 16:30:49 +0300

    almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,15 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+    let counter: number = 0;
 
+    for (let i: number = 0; i < sequence.length - 1; i++) {
+        counter += sequence[i] < sequence[i + 1] ? 0 : 1;
+        if (counter > 1) {
+            return false;
+        }
+    }
+
+    return counter <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 1, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
