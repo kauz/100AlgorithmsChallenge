8d5c9aa10e8802b7d660341823dce795dd8cfbbc
kauz <kauzlein@gmail.com>
2019-02-01 19:04:12 +0300

    alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,13 @@
 function alternatingSums(a: number[]): number[] {
+    let weights: number[] = [0, 0];
+    let goesInFirstTeam: boolean = true;
 
+    for (let i: number = 0; i < a.length; i++) {
+        goesInFirstTeam ? weights[0] += a[i] : weights[1] += a[i];
+        goesInFirstTeam = !goesInFirstTeam;
+    }
+
+    return weights;
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
