021505f1e4e3ac01e403a33cebbce87e02dd9109
kauz <kauzlein@gmail.com>
2019-02-02 16:16:25 +0300

    arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,39 @@
 function arrayChange(inputArray: number[]): number {
- 
+
+    let counter = 0;
+
+    for (let i: number = 0; i < inputArray.length; i++) {
+     for (let j: number = i + 1; j <= inputArray.length; j++) {
+            if (inputArray[j] <= inputArray[i]) {
+                inputArray[j] += 1;
+                counter++;
+            } else {
+                break;
+            }
+        }
+    }
+
+    console.log(inputArray);
+    return counter;
 }
 
-console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+
+
+// VARIANT 2
+
+// function arrayChange(inputArray: number[]): number {
+//
+//     let counter = 0;
+//
+//     for (let i: number = 0; i < inputArray.length; i++) {
+//         if (inputArray[i] >= inputArray[i + 1]) {
+//             let diff = (inputArray[i] + 1) - inputArray[i + 1];
+//             inputArray[i + 1] = inputArray[i] + 1;
+//             counter += diff;
+//         }
+//     }
+//
+//     console.log(inputArray);
+//     return counter;
+// }
