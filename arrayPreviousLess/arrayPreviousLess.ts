38c299b2a4c8130112c66500ba5eab6afe3dab20
kauz <kauzlein@gmail.com>
2019-02-03 14:48:02 +0300

    arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,18 @@
 function arrayPreviousLess(items: number[]): number[] {
+    let resArr: number[] = [];
 
+    for (let i = items.length - 1; i >= 0; i--) {
+        for (let j = i; j >= 0; j--) {
+            if (items[i] > items[j]) {
+                resArr.unshift(items[j]);
+                break;
+            } else if (j === 0) {
+                resArr.unshift(-1);
+            }
+        }
+    }
+
+   return resArr;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
