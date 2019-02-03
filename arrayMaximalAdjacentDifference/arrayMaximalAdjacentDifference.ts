8c7e0d94f7d3a8730f20bb3945c4e0bded5f6d6f
kauz <kauzlein@gmail.com>
2019-02-03 14:31:49 +0300

    arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,16 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let resArr: number[] = [];
 
+    for (let i: number = 0; i < inputArray.length; i++) {
+
+        if (i === 0) {
+            resArr.push(Math.abs(inputArray[i] - inputArray[i + 1]))
+        } else {
+            resArr.push(Math.abs(inputArray[i] - inputArray[i - 1]));
+        }
+    }
+
+    return Math.max(...resArr);
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
