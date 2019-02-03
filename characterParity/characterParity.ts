a9e57f9781cabfc320fe87f8c5422c24290c820a
kauz <kauzlein@gmail.com>
2019-02-03 22:19:22 +0300

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,14 @@
 function characterParity(symbol: string): string {
-   
+
+    let parsed: number = parseInt(symbol);
+
+    return isNaN(parsed)
+        ? 'not a digit'
+        : parsed % 2 === 0
+            ? 'even'
+            : 'odd';
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
