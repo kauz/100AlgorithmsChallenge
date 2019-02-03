720f9f648ab15dc01bc5296091fdc6c6a22438ca
kauz <kauzlein@gmail.com>
2019-02-03 14:08:55 +0300

    arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,13 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+function arrayMaxConsecutiveSum(inputArray: number[], k: number): number | null {
+    let kArr: number[] = [];
+    for (let i: number = 0; i < inputArray.length - 1; i++) {
+        if (inputArray[i + k - 1]) {
+            let sliced: number[] = inputArray.slice(i, i + k);
+            kArr.push(sliced.reduce((accumulator: number, currentValue: number) => accumulator + currentValue, 0));
+        }
+    }
 
+    return kArr.length ? Math.max(...kArr) : null;
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
