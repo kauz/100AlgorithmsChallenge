7fba92dd132d4bbc3bebfd18e3d618a5fbed8775
kauz <kauzlein@gmail.com>
2019-02-03 22:09:15 +0300

    centuryFromYear

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,5 +1,11 @@
 function centuryFromYear(year: number): number {
- 
+    let century = year / 100;
+    return (
+        century !== Math.floor(century)
+            ? Math.floor(century) + 1
+            : century
+    );
+
 }
 
 console.log(centuryFromYear(1905));
