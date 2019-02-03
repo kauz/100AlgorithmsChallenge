2e62a5ba2fc7e9b13ceede14c3812bd0cbf64986
kauz <kauzlein@gmail.com>
2019-02-03 21:19:52 +0300

    caseInsensitivePalimdrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,6 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    let palindrome = inputString.toLowerCase().split('');
+    return palindrome.join() === palindrome.reverse().join();
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
