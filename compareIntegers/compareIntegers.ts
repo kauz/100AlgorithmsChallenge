2302cf6a352847d4475cf6ca45de387ed68bbe03
kauz <kauzlein@gmail.com>
2019-02-04 20:24:28 +0300

    compareIntegers

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,19 @@
 function compareIntegers(a: string, b: string): string {
+    let aNum = parseInt(a);
+    let bNum = parseInt(b);
 
+    if (isNaN(aNum) || isNaN(bNum)) {
+        return 'bullshit';
+    }
+
+    return aNum === bNum
+        ? 'equal'
+        : aNum < bNum
+            ? 'less'
+            : 'greater';
 }
 
+console.log(compareIntegers('n', '13'));
 console.log(compareIntegers('12', '13'));
 console.log(compareIntegers('875', '799'));
 console.log(compareIntegers('1000', '1000'));
