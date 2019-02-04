b82112fc5c48348db78d0322c42f4cc08fef343c
kauz <kauzlein@gmail.com>
2019-02-04 22:57:14 +0300

    depositProfit

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,13 @@
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+    let year: number = 0;
+
+    while (deposit < threshold) {
+        deposit += Math.round((rate / 100) * deposit);
+        year++;
+        console.log(deposit);
+    }
+
+    return year;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
