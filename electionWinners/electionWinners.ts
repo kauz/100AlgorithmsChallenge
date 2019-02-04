b89f1ef40a1f36eb4b0bb7a33120a9bc8fa7d648
kauz.th <kauzlein@gmail.com>
2019-02-04 16:55:53 +0300

    electionWinners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,37 @@
 function electionsWinners(votes: number[], k: number): number {
+    let possibleCandidates = 0;
 
+    votes.sort((a, b) => a - b).forEach((candidate: number, idx: number, candidates: number[])  => {
+        if (candidate) {
+            if (candidate + k > Math.max(...candidates) || (candidate === Math.max(...candidates) && candidate !== Math.min(...candidates))) {
+                possibleCandidates++;
+            }
+        }
+    });
+
+    return possibleCandidates;
 }
 
+
+// function electionsWinners(votes: number[], k: number): number {
+//     let inTheRunning = 0;
+//     let mostVotes = Math.max(...votes);
+//     let sortedVotes = votes.sort((a, b) => b - a);
+//
+//     if (sortedVotes[0] !== sortedVotes[1] && k === 0) {
+//         return 1;
+//     }
+//
+//     votes.forEach((voteCount: number)  => {
+//         if (mostVotes - voteCount < k) {
+//             inTheRunning++;
+//         }
+//     });
+//
+//     return inTheRunning;
+// }
+
 console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([1, 4, 5, 10], 20));
+console.log(electionsWinners([1, 4, 5, 10], 0));
+console.log(electionsWinners([1, 1, 1, 1], 0));
