b88fed8cb804db4e1003c2f6a384613dbe56ade0
kauz.th <kauzlein@gmail.com>
2019-02-04 17:43:23 +0300

    factorializeANumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,19 @@
 function factorializeANumber(num: number): number {
+    let sequence: number[] = [];
+    let counter = 1;
 
+    while (true) {
+        if (counter > Math.abs(num)) {
+            break;
+        } else {
+            sequence.push(counter);
+            counter++;
+        }
+    }
+
+    // console.log(sequence.reduce((a, b) => `${a} ${b}`, `${num}! =`));
+    return sequence.reduce((a, b) => a * b, 1);
 }
 
-console.log(factorializeANumber(5));
+console.log(factorializeANumber(-5));
 console.log(factorializeANumber(10));
