4a85c11e8e820f467e4744fdc048b3779060efa8
kauz.th <kauzlein@gmail.com>
2019-02-04 18:22:19 +0300

    fancyRide

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,30 @@
 function fancyRide(l: number, fares: number[]): string {
+    const BONUS_MONEY = 20;
+    const RIDES: string[] = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
 
+    let index = 0;
+
+    fares.forEach((fare, i, arr) => {
+        if (fare === Math.max(...arr.filter(n => n * l < BONUS_MONEY))) {
+            index = i;
+        }
+    });
+
+    return RIDES[index];
 }
+
+
+// function fancyRide(l: number, fares: number[]): string {
+//     const BONUS_MONEY = 20;
+//     const RIDES: string[] = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
+//
+//     for (let i = fares.length - 1; i >= 0; i--) {
+//         if (fares[i] * l <= BONUS_MONEY) {
+//             return RIDES[i];
+//         }
+//     }
+//
+// }
+
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
