b49413dfa0a23f6866d8abbc126aa0e6f3b6a6f2
kauz <kauzlein@gmail.com>
2019-02-04 22:34:14 +0300

    crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,21 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+    let aRowSum = 0;
+    let bColSum = 0;
+
+    matrix.forEach((arr, i) => {
+        bColSum += arr[b];
+        if (i === a) {
+            arr.forEach((n, i) => {
+                if (i !== b) {
+                    aRowSum += n;
+                }
+            });
+        }
+    });
+
+    return aRowSum + bColSum;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
+console.log(crossingSum([[1, 1, 1, 1],
+    [2, 2, 2, 2],
     [3, 3, 3, 3]], 1, 3));
