9cad10e1e2c156460d41bd828d8f6717f315e168
kauz.th <kauzlein@gmail.com>
2019-02-04 17:11:18 +0300

    extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,19 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+    let res: number[] = [];
 
+    // matrix.forEach(arr => {
+    //     arr.forEach((n, i) => {
+    //         if (i === column) {
+    //             res.push(n);
+    //         }
+    //     });
+    // });
+
+    matrix.forEach(arr => {
+        res.push(arr[column]);
+    });
+
+    return res;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
