4aa2d957f977a1cf210a542380c8c9af6d1dd4df
kauz <kauzlein@gmail.com>
2019-02-04 21:24:08 +0300

    containsDuplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,13 @@
 function containsDuplicates(a: number[]): boolean {
+    let truthy = false;
 
+    a.sort((a, b) => a - b).forEach((n: number, i: number, arr: number[]) => {
+        if (n === arr[i + 1]) {
+            truthy = true;
+        }
+    });
+
+    return truthy;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
