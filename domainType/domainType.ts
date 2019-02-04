1c4c4788c229750f419996151b10e8480d4197dc
kauz.th <kauzlein@gmail.com>
2019-02-04 16:19:21 +0300

    domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,27 @@
-function domainType(domains: string[]): string[] {
-
+interface types {
+    [key: string]: string;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+function domainType(domains: string[]): string[] {
+    let domainTypes: types = {
+        'com': 'commercial',
+        'org': 'organization',
+        'net': 'network',
+        'info': 'information'
+    };
+
+    let res: string[] = [];
+
+    domains.forEach(url => {
+        let tmp = url.split('.');
+        if (domainTypes[tmp[tmp.length - 1]]) {
+            res.push(domainTypes[tmp[tmp.length - 1]]);
+        } else {
+            res.push('other');
+        }
+    });
+
+    return res;
+}
+
+console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info", "govnokod.ru"]));
