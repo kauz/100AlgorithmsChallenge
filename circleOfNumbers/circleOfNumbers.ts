acad52d800a141e1c99e200b4ebf78a63fab6571
kauz <kauzlein@gmail.com>
2019-02-04 19:21:01 +0300

    circleOfNumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,12 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
+    let res: number[] = [];
+    let opposite = firstNumber < n / 2 ? firstNumber + n / 2: firstNumber - n / 2;
 
+    for (let i = 0; i < n; i++) {
+        res.push(i);
+    }
+
+    return res[opposite];
 }
 
 console.log(circleOfNumbers(10, 2));
