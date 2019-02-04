32e630a48d58879d5c3436978ddc61a7488d0cfa
kauz <kauzlein@gmail.com>
2019-02-04 23:11:20 +0300

    differentSymbolsNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,15 @@
 function differentSymbolsNaive(s: string): number {
- 
+    return s.split('').sort().filter((n, i, arr) => n !== arr[i + 1]).length;
 }
+
+// with new Set();
+// function differentSymbolsNaive(s: string): number {
+//     let unique = new Set();
+//     s.split('').forEach(c => {
+//        unique.add(c);
+//     });
+//
+//     return unique.size;
+// }
 
 console.log(differentSymbolsNaive('cabca'));
