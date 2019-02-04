6dfee4680d049cfa0f131c6abf5497327adf965b
kauz <kauzlein@gmail.com>
2019-02-04 20:19:56 +0300

    companyBotStrategy

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,5 +1,18 @@
 function companyBotStrategy(trainingData: number[][]): number {
- 
+
+    let correctAnswers = trainingData.filter((subArr: number[]) => subArr[1] === 1);
+
+    if (!correctAnswers.length) {
+        return correctAnswers.length;
+    }
+
+    let sum = 0;
+
+    correctAnswers.forEach((subArr: number[]) => {
+        sum += subArr[0];
+    });
+
+    return sum / correctAnswers.length;
 }
 
 console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
