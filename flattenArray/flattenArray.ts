d5d1ee3c33fc1493eda8618e529cb213ff994054
kauz.th <kauzlein@gmail.com>
2019-02-05 16:19:11 +0300

    flattenArray

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,21 @@
 function flattenArray(arr: any[]): any[] {
+    let newArr: any[] = [];
 
+    flatten(arr);
+
+    function flatten(arr: any[]) {
+        arr.forEach((el) => {
+            if (Array.isArray(el)) {
+                flatten(el);
+            } else {
+                newArr.push(el);
+            }
+        });
+    }
+
+    return newArr;
 }
 
 console.log(flattenArray([[["a"]], [["b"]]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([['a']]));
