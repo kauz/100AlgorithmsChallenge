183028c7a7698d12ebbf487bbada37e97454b123
kauz.th <kauzlein@gmail.com>
2019-02-05 14:40:58 +0300

    firstDigit

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,16 @@
 function firstDigit(inputString: string): string {
+    let strArr = inputString.split('');
 
+    for (let i = 0; i < strArr.length; i++) {
+        if (!isNaN(Number(strArr[i]))) {
+            return strArr[i];
+        }
+    }
+
+    return '';
 }
 
 console.log(firstDigit('var_1__Int'));
 console.log(firstDigit('q2q-q'));
 console.log(firstDigit('0ss'));
+console.log(firstDigit('sss'));
