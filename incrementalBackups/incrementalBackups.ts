8727664768d02776448f0f5d1dea4e14c0cf97af
kauz.th <kauzlein@gmail.com>
2019-02-05 17:56:17 +0300

    incrementalBackups

--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,11 +1,19 @@
 function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
+    let ids: number[] = [];
 
+    for (let i = 0; i < changes.length; i++) {
+        if (changes[i][0] > lastBackupTime && !ids.includes(changes[i][1])) {
+            ids.push(changes[i][1]);
+        }
+    }
+
+    return ids.sort(((a, b) => a - b));
 }
 
-console.log(incrementalBackups(461620205, [[461620203, 1], 
-    [461620204, 2], 
+console.log(incrementalBackups(461620205, [[461620203, 1],
+    [461620204, 2],
     [461620205, 6],
-    [461620206, 5], 
-    [461620207, 3], 
-    [461620207, 5], 
+    [461620206, 5],
+    [461620207, 3],
+    [461620207, 5],
     [461620208, 1]]));
