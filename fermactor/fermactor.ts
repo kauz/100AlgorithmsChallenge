8842862c5167182186027a2552991c7c7e598cba
kauz.th <kauzlein@gmail.com>
2019-02-05 13:23:55 +0300

    fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,16 @@
 function fermactor(n: number): number[] {
 
+    if (n > 0) {
+        for (let x = 1; x < n; x++) {
+            for (let y = 1; y < x; y++) {
+                if ((x - y) * (x + y) === n) {
+                    return [x, y];
+                }
+            }
+        }
+    }
+
+    return [0];
 }
 
 console.log(fermactor(15));
