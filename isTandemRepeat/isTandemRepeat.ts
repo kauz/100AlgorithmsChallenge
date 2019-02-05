ee1ded5a504a1cb51293549b8e216c92e75c5934
kauz.th <kauzlein@gmail.com>
2019-02-05 18:51:26 +0300

    isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,7 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+    return inputString.substr(0 ,inputString.length / 2) === inputString.substr(inputString.length / 2);
 }
 
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'));
+console.log(isTandemRepeat('qqq'));
+console.log(isTandemRepeat('2w2ww'));
