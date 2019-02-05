135b38ce9d43ba10bdc2279c4ba4c32984c5f32f
kauz.th <kauzlein@gmail.com>
2019-02-05 17:28:52 +0300

    htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,26 @@
 function htmlEndTagByStartTag(startTag: string): string {
+    let res: string = '';
 
+    if (startTag.charAt(0) !== '<' || startTag.charAt(startTag.length - 1) !== '>') {
+        return res;
+    }
+
+    for (let i = 0; i < startTag.length; i++) {
+
+        if (startTag[i] === ' ') {
+            res += '>';
+            break;
+        }
+
+        res += startTag[i];
+
+        if (startTag[i] === '<') {
+            res += '/';
+        }
+    }
+
+    return res;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'))
+console.log(htmlEndTagByStartTag('<i>'));
