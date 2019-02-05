f7634d5b0633570aebe1ebaab957b418b7868b7d
kauz.th <kauzlein@gmail.com>
2019-02-05 15:05:20 +0300

    firstDuplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,30 @@
+// interface uniqueNums {
+//     [key:number]: number;
+// }
+//
+// function firstDuplicate(a: number[]): number {
+//     let unique: uniqueNums = {};
+//
+//     for (let n of a) {
+//         if (unique.hasOwnProperty(n)) {
+//             return n;
+//         }
+//         unique[n] = n;
+//     }
+//     return -1;
+// }
+
 function firstDuplicate(a: number[]): number {
-    
+
+    for (let i = 0; i < a.length; i++) {
+        for (let j = i + 1; j < a.length - 1; j++) {
+            if (a[i] === a[j]) {
+                return a[j];
+            }
+        }
+    }
+
+    return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
