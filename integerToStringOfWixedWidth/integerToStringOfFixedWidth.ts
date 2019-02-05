58126643612f62eb2f39111be42a2b70e98714b1
kauz.th <kauzlein@gmail.com>
2019-02-05 18:06:37 +0300

    integerToStringOfWixedWidth

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,13 @@
 function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+    let numStr: string = number.toString();
+
+    if (numStr.length < width) {
+        numStr = '0'.repeat(width - numStr.length) + numStr;
+    } else if (numStr.length > width) {
+        numStr = numStr.slice(width - numStr.length);
+    }
+
+    return numStr;
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));
