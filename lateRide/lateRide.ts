35f096d3328076aa48d47da3452f5573d3f7c3b5
= <=>
2019-02-06 00:06:43 +0300

    lateRide

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,14 @@
 function lateRide(n: number): number {
+    let minutes = n % 60;
+    let hours = (n - minutes) / 60;
 
+    if (hours > 24) {
+        hours = hours % 24;
+    }
+
+    return hours.toString().concat(minutes.toString()).split('').reduce((a, b) => a + Number(b), 0);
 }
 
 console.log(lateRide(240));
 console.log(lateRide(808));
+console.log(lateRide(8000));
