e207ff405149121a7c68d0770647be685200be3a
kauz.th <kauzlein@gmail.com>
2019-02-05 16:33:14 +0300

    growingPlant

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,16 @@
 function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    let height: number = 0;
+    let days: number = 0;
 
+    while (height < desiredHeight) {
+        height += upSpeed;
+        days++;
+        if (height < desiredHeight) {
+            height -= downSpeed;
+        }
+    }
+
+    return days;
 }
 
-console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
