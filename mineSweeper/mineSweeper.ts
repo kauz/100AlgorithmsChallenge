cd70a3a212637db9894dde3893aff3ed79735a2a
= <=>
2019-02-06 12:58:58 +0300

    longestDigitsPrefix

--- mineSweeper/mineSweeper.ts
+++ mineSweeper/mineSweeper.ts
@@ -1,7 +1,45 @@
 function minesweeper(matrix: boolean[][]): number[][] {
-  
+    let setup: number[][] = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        setup.push([]);
+        for (let j = 0; j < matrix[i].length; j++) {
+            let cnt: number = 0;
+            if (i > 0) {
+                if (matrix[i - 1][j]) {
+                    cnt += 1;
+                }
+                if (matrix[i - 1][j + 1]) {
+                    cnt += 1;
+                }
+                if (matrix[i - 1][j - 1]) {
+                    cnt += 1;
+                }
+            }
+            if (i < matrix.length - 1) {
+                if (matrix[i + 1][j]) {
+                    cnt += 1;
+                }
+                if (matrix[i + 1][j + 1]) {
+                    cnt += 1;
+                }
+                if (matrix[i + 1][j - 1]) {
+                    cnt += 1;
+                }
+            }
+
+            if (matrix[i][j + 1]) {
+                cnt += 1;
+            }
+            if (matrix[i][j - 1]) {
+                cnt += 1;
+            }
+            setup[i].push(cnt)
+        }
+    }
+    return setup
 }
 
 console.log(minesweeper([[true, false, false],
-[false, true, false],
-[false, false, false]]));
+    [false, true, false],
+    [false, false, false]]));
