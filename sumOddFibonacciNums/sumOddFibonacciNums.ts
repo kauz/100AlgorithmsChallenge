24355b39c7559be0e744db15e3794c2aaac7c447
kauz.th <kauzlein@gmail.com>
2019-02-07 16:16:09 +0300

    sumOddFibonacciNums

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,5 +1,19 @@
 function sumOddFibonacciNums(num: number): number {
+    let a = 1, b = 1;
+    let c = 0;
+    let sum = 0;
 
+    for (let i = 3; c < num; i++) {
+        a = b;
+        b = c;
+        c = a + b;
+
+        if (c < num && c % 2 !== 0) {
+            sum += c;
+        }
+    }
+
+    return sum;
 }
 
 console.log(sumOddFibonacciNums(1000));
