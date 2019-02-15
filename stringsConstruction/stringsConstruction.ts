8c3ea33764bed84f387f1f2bb70173fd767273f9
kauz.th <kauzlein@gmail.com>
2019-02-15 16:30:06 +0300

    stringsConstruction

--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,18 @@
 function stringsConstruction(a: string, b: string): number {
 
+    let A: any = {};
+    a.split('').forEach(char => {A[char] = 0});
+    let B: string[] = b.split('');
+
+    for (let i = 0; i < B.length; i++) {
+        if (A.hasOwnProperty(B[i])) {
+            A[B[i]]++;
+        }
+    }
+
+    // @ts-ignore
+    return Math.min(...Object.values(A));
 }
 
 console.log(stringsConstruction('abc', 'abccba'));
+console.log(stringsConstruction('abc', 'abccbacccc'));
