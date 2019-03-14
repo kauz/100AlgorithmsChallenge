3e3149d3a19d8fe4c4003963932e40397d5d5a78
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-14 16:43:27 +0200

    longestDigitsPrefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,9 @@
 function longestDigitsPrefix(inputString: string): string {
+  let strArr = inputString.split(/\D/).filter(n => n !== '');
 
+  return strArr.reduce((a, b) => a.length > b.length ? a : b, '')
 }
 
 console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1df3452345'));
+console.log(longestDigitsPrefix('123aa1df9999999fd324523sd098#$lsd'));
