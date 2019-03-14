f4c7600106650e20b7476d0f19e3c2b9d2c5e574
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-14 16:30:51 +0200

    launchSequenceChecker

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,5 +1,22 @@
-function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+interface iCode {
+  [key:string]: number;
+}
 
+function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+  const launchCodes: iCode = {};
+
+  for (let i = 0; i < systemNames.length; i++) {
+    if (launchCodes.hasOwnProperty(systemNames[i])) {
+      if(launchCodes[systemNames[i]] >= stepNumbers[i]) {
+        return false;
+      }
+      launchCodes[systemNames[i]] = stepNumbers[i];
+    } else {
+      launchCodes[systemNames[i]] = stepNumbers[i];
+    }
+  }
+
+  return true;
 }
 
 console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
