0778c5735d2915e8e75a7e62f01581c565fcb126
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-15 15:44:20 +0200

    makeArrayConsecutive2

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,17 @@
 function makeArrayConsecutive2(statues: number[]): number {
+  let counter = 0;
+  let sortedStatues = statues.sort((a, b) => a - b);
+  const min = sortedStatues[0];
+  const max = sortedStatues[sortedStatues.length - 1];
 
+  for(let i = min; i < max; i++) {
+    if(!statues.includes(i)) {
+      statues.push(i);
+      counter++
+    }
+  }
+
+  return counter;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
