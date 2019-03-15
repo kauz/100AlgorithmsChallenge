a31c0d1837c7f0b2192a0dc8f7bac12d1edec16a
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-15 15:52:38 +0200

    matrixElementsSum

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,5 +1,18 @@
 function matrixElementsSum(matrix: any[][]): number {
+    let totalPrice = 0;
 
+    matrix.forEach((arr, arrIdx) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 0) {
+                continue;
+            } else if (matrix[arrIdx - 1] && matrix[arrIdx - 1][i] === 0) {
+                continue;
+            }
+            totalPrice += arr[i];
+        }
+    });
+
+    return totalPrice;
 }
 
 console.log(matrixElementsSum(
