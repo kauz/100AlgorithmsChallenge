28905081cf7a2bd4bcea802316d954d42dd0b84b
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-15 17:50:11 +0200

    mostFrequentDigitSum

--- mostFrequentDigitSum/mostFrequentDigitSum.ts
+++ mostFrequentDigitSum/mostFrequentDigitSum.ts
@@ -1,5 +1,25 @@
-function mostFrequentDigitSum(n: number): number {
+interface iCounter {
+  [key: string]: number
+}
 
+function mostFrequentDigitSum(n: number): number {
+  let frequency: iCounter = {};
+
+  for (let i = n; i > 0; i = i - step(i)) {
+    if (frequency[step(i)]) {
+      frequency[step(i)]++;
+    } else {
+      frequency[step(i)] = 1;
+    }
+  }
+
+  console.log(frequency);
+  return Object.keys(frequency).reduce((k, nk) => frequency[k] > frequency[nk] ? Number(k) : Number(nk), 0)
+
+}
+
+function step(i: number) {
+  return String(i).split('').reduce((a, b) => a + Number(b), 0);
 }
 
 console.log(mostFrequentDigitSum(88));
