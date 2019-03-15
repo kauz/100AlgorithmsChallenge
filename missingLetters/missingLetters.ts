9165a2746fb8d309b7e2b8786d5f3c2924615644
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-15 17:17:24 +0200

    missingLetters

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,7 +1,21 @@
-function missingLetters(str: string): string {
-   
+function missingLetters(str: string): string | null | undefined{
+  let cstr = str.toLowerCase()
+    .split('')
+    .map((c) => c.charCodeAt(0))
+    .sort((a, b) => a - b)
+    .filter(n => !(n < 97 || n > 122));
+  let iterations = cstr.length;
+
+  for (let i = 0; i < iterations; i++) {
+    if (cstr[i + 1] && cstr[i + 1] - 1 !== cstr[i]) {
+      return String.fromCharCode(cstr[i] + 1);
+    }
+  }
+
+  return undefined;
 }
 console.log(missingLetters("bce"));
-console.log(missingLetters("abce"));
+console.log(missingLetters("ABCE"));
+console.log(missingLetters("1A##@@B=CE"));
 console.log(missingLetters("abcdefghjklmno"));
 console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
