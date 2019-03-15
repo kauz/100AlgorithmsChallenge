0b630b7bf2964a0d234b835feafdb509240f4532
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-15 16:11:08 +0200

    maxMultiple

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,22 @@
 function maxMultiple(divisor: number, bound: number): number  {
 
+  // 1st solution
+
+  // let n = divisor;
+  //
+  // for (let i = bound; i > 0; i--) {
+  //   if(i % divisor === 0) {
+  //     return i;
+  //   }
+  // }
+  //
+  // return n;
+
+
+  // 2nd solution
+
+  let remainder = bound % divisor;
+  return bound - remainder;
 }
 
 console.log(maxMultiple(3,10));
