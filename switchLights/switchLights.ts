bec7b0bff8fe5f23d0594b9ed245d6548203370d
= <=>
2019-03-16 11:32:34 +0300

    switchLights

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,5 +1,23 @@
 function switchLights(a: number[]): number[] {
 
+    let mutatedLights = [...a];
+
+    for (let i = 0; i < a.length; i++) {
+        if(a[i]) {
+            mutatedLights = swap(mutatedLights, i)
+        }
+    }
+
+    return mutatedLights;
+}
+
+function swap(lights: number[], curIdx: number): number[] {
+
+    for (let i = 0; i < curIdx + 1; i++) {
+        lights[i] = lights[i] ? 0 : 1;
+    }
+
+    return lights;
 }
 
 
