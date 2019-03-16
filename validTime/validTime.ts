05d8f708259bce7c41760d10a7c57814a2a83206
= <=>
2019-03-16 11:15:23 +0300

    validTime

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,5 +1,11 @@
 function validTime(time: string): boolean {
+    let [hours, minutes] = time.split(':').map(c => parseInt(c));
 
+    if (!hours || !minutes) {
+        return false;
+    }
+
+    return !((hours > 23 || hours < 0) || (minutes > 60 || minutes < 0));
 }
 
 console.log(validTime('13:58'));
