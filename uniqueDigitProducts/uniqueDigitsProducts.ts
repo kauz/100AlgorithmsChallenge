5a1952fced9703872fd5d759111514a7ff9854d8
= <=>
2019-03-16 11:43:50 +0300

    uniqueDigitProducts

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,18 @@
 function uniqueDigitProducts(a: number[]): number {
+    let tmp: number[] = [];
+    for (let n of a) {
+        if (n.toString().length === 1) {
+            tmp.push(n);
+        } else {
+            tmp.push(n.toString().split('').reduce((a, b) => a * Number(b), 1));
+        }
+    }
+
+    return tmp.filter(onlyUnique).length;
+}
+
+function onlyUnique(value: number, index: number, self: number[]) {
+    return self.indexOf(value) === index;
 }
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
