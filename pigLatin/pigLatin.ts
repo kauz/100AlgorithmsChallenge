fd58fbe287cf517c456f1d108d9d09cddcf74d9c
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-18 14:22:31 +0200

    pigLatin

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,18 @@
 function pigLatin(str: string): string {
+  str = str.toLowerCase();
+  const consonantsReg = () => /^(?=[a-z])[^aeiouy]+/; // The letter Y can represent a consonant / semivowel (yes, yard) or a vowel (mystery, try, play).
+  let consonant = consonantsReg().exec(str);
 
+  if (!consonant) {
+      str += 'way';
+  } else {
+    console.log(consonant);
+    str = str.substr(consonant[0].length) + consonant[0] + 'ay';
+  }
+
+  return str;
 }
 
 console.log(pigLatin("glove"));
 console.log(pigLatin("eight"));
+
