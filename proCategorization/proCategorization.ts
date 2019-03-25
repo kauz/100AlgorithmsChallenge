525f3d267cff7dc993ea1388b6577195be06f56f
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-25 18:26:04 +0200

    proCategorization

--- proCategorization/proCategorization.ts
+++ proCategorization/proCategorization.ts
@@ -1,5 +1,38 @@
-function proCategorization(pros: string[], preferences: string[][]): string[][][] {
+interface pro {
+  id: number,
+  name: string,
+  specialties: string[]
+}
 
+function proCategorization(pros: string[], preferences: string[][]): string[][][] {
+  const professionals: pro[] = [];
+  let specialties: string[] = [];
+
+  for (let i = 0; i < pros.length; i++) {
+    professionals.push({id: i + 1, name: pros[i], specialties: preferences[i]});
+    specialties.push(...preferences[i].filter(v => !specialties.includes(v)));
+  }
+
+  let res: string[][][] = [];
+
+  specialties.forEach(val => {
+    res.push([[val], getPref(professionals, val)])
+  });
+
+  return res;
+}
+
+function getPref(objs: pro[], name: string): string[] {
+  let names: string[] = [];
+
+  objs.forEach(pro => {
+    if (pro.specialties.includes(name)) {
+      names.push(pro.name);
+    }
+
+  });
+
+  return names;
 }
 
 console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
