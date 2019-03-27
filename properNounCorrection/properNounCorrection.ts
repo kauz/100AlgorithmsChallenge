3673cb7050b8e1b7c39019e8b32c65ff6177a0f5
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-27 12:16:17 +0200

    properNounCorrection

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,5 +1,7 @@
 function properNounCorrection(noun: string): string {
-   
+   const nounArr = noun.toLowerCase().split('');
+   nounArr[0] = nounArr[0].toUpperCase();
+   return nounArr.join('');
 }
 
 console.log(properNounCorrection('pARiS'));
