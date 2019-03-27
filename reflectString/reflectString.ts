7f3e39b7f92fb75609521e306f5b0c9e351345ff
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-27 13:39:51 +0200

    reflectString

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,13 @@
 function reflectString(inputString: string): string {
-  
+  const alpabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let inputArr = inputString.toLowerCase().split('');
+
+  inputArr = inputArr.map((c: string) => {
+    const idx = alpabet.indexOf(c);
+    return idx !== -1 ? alpabet.reverse()[idx] : c;
+  });
+
+  return inputArr.join('');
 }
 
 console.log(reflectString("name"));
