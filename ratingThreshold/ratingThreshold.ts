48b4d946cd1bd7162bb1df7a2db57e7cce64654d
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-27 12:49:38 +0200

    ratingThreshold

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,21 @@
 function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+    const indexes: number[] = [];
+    const avg = (arr: number[]): number => {
+        const sum = arr.reduce((a, b) => a + b, 0);
+        return sum / arr.length;
+    };
 
+    ratings.forEach((arr, idx) => {
+        if (avg(arr) < threshold) {
+            indexes.push(idx);
+        }
+    });
+
+    return indexes;
 }
 
 console.log(ratingThreshold(3.5, 
     [[3, 4],
     [3, 3, 3, 4],
-    [4]]));
+    [4],
+    [1, 1]]));
