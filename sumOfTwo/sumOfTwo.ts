9a00facb3bdcaf93f6588e9f87802269e8d634d2
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-28 14:43:07 +0200

    sumOfTwo

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,15 @@
 function sumOfTwo(a: number[], b: number[], v: number): boolean {
+  const iLen = a.length, jLen = b.length;
 
+  for (let i = 0; i < iLen; i++) {
+    for (let j = 0; j < jLen; j++) {
+      if (a[i] + b[j] === v) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
