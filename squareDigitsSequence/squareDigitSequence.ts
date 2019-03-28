1722ff5a8e6ed00812fb9b8f2ae4e36d729f4bb1
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-28 11:09:27 +0200

    squareDigitsSequence

--- squareDigitsSequence/squareDigitSequence.ts
+++ squareDigitsSequence/squareDigitSequence.ts
@@ -1,5 +1,28 @@
 function squareDigitsSequence(a0: number): number {
+  let sequence: number[] = [a0];
+  let idx = 0;
 
+  while(true) {
+    let curNum = 0;
+
+    sequence[idx].toString()
+      .split('')
+      .filter((c) => c !== '0')
+      .map((a) => {
+        curNum += Math.pow(Number(a), 2);
+      });
+
+    if (sequence.includes(curNum)) {
+      sequence.push(curNum);
+      break;
+    }
+
+    sequence.push(curNum);
+    idx++;
+  }
+
+  console.log(sequence);
+  return sequence.length;
 }
 
 
