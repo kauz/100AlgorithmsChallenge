ee90eaa78e24ae61e4f828161df13e675a1607a9
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-28 11:09:01 +0200

    sortByHeight

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,18 @@
 function sortByHeight(a: number[]): number[] {
+  let res: number[] = [];
+  let filtered = a.filter(n => n !== -1 || isNaN(n)).sort((a, b) => a - b);
+  let j = 0;
 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === -1) {
+      res[i] = a[i];
+    } else {
+      res[i] = filtered[j];
+      j++;
+    }
+  }
+
+  return res;
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
