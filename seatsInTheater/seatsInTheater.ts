776a74b6ff14507a4899bf962bda1c182505bf7b
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-28 11:08:26 +0200

    seatsInTheater

--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,5 @@
 function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
-
+  return (nRows - row) * (nCols - col + 1);
 }
 
 console.log(seatsInTheater(16, 11, 5, 3));
