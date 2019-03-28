68868ec729027f6cbbacf38d0cc2dcd138e6cb38
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-28 15:05:34 +0200

    minimalNumberOfCoins

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,22 @@
 function minimalNumberOfCoins(coins: number[], price: number): number {
+  let counter = 0;
 
+  for (let i = price; i > 0; i = greedySpendCoin(i, coins)) {
+    counter++;
+  }
+
+  return counter;
+}
+
+function greedySpendCoin(currentStep: number, availableCoins: number[]): number {
+  let maxCoin = Math.max(...availableCoins);
+  if (currentStep < maxCoin) {
+    availableCoins.pop();
+    maxCoin = Math.max(...availableCoins);
+  }
+
+  return currentStep - maxCoin;
 }
 
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 10], 28));
