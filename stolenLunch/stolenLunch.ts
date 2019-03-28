6069a62f46a6e5887f84fc3e45bbdfe6286bd599
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-28 14:36:10 +0200

    stolenLunch

--- stolenLunch/stolenLunch.ts
+++ stolenLunch/stolenLunch.ts
@@ -1,5 +1,20 @@
 function stolenLunch(note: string): string {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let digits = '0123456789';
+  let len = note.length;
+  const decipher = note.split('');
 
+  for (let i = 0; i < len; i++) {
+    const alphIdx = alphabet.indexOf(note[i]);
+    const digIdx = digits.indexOf(note[i]);
+    if (alphIdx !== -1) {
+      decipher[i] = digits[alphIdx] || note[i];
+    } else if (digIdx !== -1) {
+      decipher[i] = alphabet[digIdx] || note[i];
+    }
+  }
+
+  return decipher.join('');
 }
 
 console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja"));
