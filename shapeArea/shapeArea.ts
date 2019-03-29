e191a8ba514c658cd7521fec1b7779df414950c0
Artyom Popov <artyom.popov@bytebrand.net>
2019-03-29 14:00:59 +0200

    shapeArea

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,6 +1,14 @@
 function shapeArea(n: number): number {
+  let area = 1;
 
+  for (let i = 1; i <= n; i++) {
+    area += ((i * 4) - 4);
+  }
+  return area;
 }
 
+console.log(shapeArea(1));
 console.log(shapeArea(2));
 console.log(shapeArea(3));
+console.log(shapeArea(4));
+console.log(shapeArea(5));
